14c8de82eaa495e2ae2f78733d1cbdb61d263cdd
Paola Casadiegos <acsapaola@gmail.com>
2023-10-20 22:58:47 -0500

    creación de carpetas y modelos de persona y usuario

---
+++ src/models/User.js
@@ -0,0 +1,10 @@
+const Person = require('./person');
+
+class User extends Person {
+    constructor(){
+        super() // Llama al constructor de la clase base
+
+    }
+
+}
+module.exports = User;
